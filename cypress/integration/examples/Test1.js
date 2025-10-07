e1d8e4fc812f8126bc0aa6f7d050165ee4853950
ryanshang <ryan.d.shang@gmail.com>
2025-10-07 16:34:10 -0400

    first test case

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,6 +1,10 @@
 // Cypress - Spec
 describe('My First Test Suite', () => {
     it('My First Test Case', () => {
-        
+        // test step
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get(".search-keyword").type('ca');
+        cy.wait(1000);
+        cy.get('.product:visible').should('have.length', 4)
     })
 })
